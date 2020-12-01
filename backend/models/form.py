0627b7cf2ca1b00af005591fb719151e4163f64d
ks129 <45097959+ks129@users.noreply.github.com>
2020-12-01 22:44:24 +0200

    Add validating emails collecting login

--- backend/models/form.py
+++ backend/models/form.py
@@ -21,4 +21,10 @@ class Form(BaseModel):
         if not all(v in FormFeatures.__members__.values() for v in value):
             raise ValueError("Form features list contains one or more invalid values.")
 
+        if (
+            FormFeatures.COLLECT_EMAIL in value
+            and FormFeatures.REQUIRES_LOGIN not in value
+        ):
+            raise ValueError("COLLECT_EMAIL feature require REQUIRES_LOGIN feature.")
+
         return value
