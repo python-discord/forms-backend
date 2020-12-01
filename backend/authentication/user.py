6a6d28038494626ed74fc842c814477b16af1e75
ks129 <45097959+ks129@users.noreply.github.com>
2020-12-01 09:54:22 +0200

    Create BaseUser implementation for JWT authentication

---
+++ backend/authentication/user.py
@@ -0,0 +1,22 @@
+import typing as t
+from abc import ABC
+
+from starlette.authentication import BaseUser
+
+
+class User(BaseUser, ABC):
+    """Starlette BaseUser implementation for JWT authentication."""
+
+    def __init__(self, token: str, payload: t.Dict) -> None:
+        self.token = token
+        self.payload = payload
+
+    @property
+    def is_authenticated(self) -> bool:
+        """Returns True because user is always authenticated at this stage."""
+        return True
+
+    @property
+    def display_name(self) -> str:
+        """Return username and discriminator as display name."""
+        return f"{self.payload['username']}#{self.payload['discriminator']}"
