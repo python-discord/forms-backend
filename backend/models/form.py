6e7ea2a30c2e1290c90fde67257fb2052d5a8ad3
ks129 <45097959+ks129@users.noreply.github.com>
2020-12-02 13:12:45 +0200

    Fix Question circular import and use cls instead self for validators

--- backend/models/form.py
+++ backend/models/form.py
@@ -3,7 +3,7 @@ import typing as t
 from pydantic import BaseModel, Field, validator
 
 from backend.constants import FormFeatures
-from backend.models import Question
+from .question import Question
 
 
 class Form(BaseModel):
@@ -14,7 +14,7 @@ class Form(BaseModel):
     questions: t.List[Question]
 
     @validator("features")
-    def validate_features(self, value: t.List[str]) -> t.Optional[t.List[str]]:
+    def validate_features(cls, value: t.List[str]) -> t.Optional[t.List[str]]:
         """Validates is all features in allowed list."""
         # Uppercase everything to avoid mixed case in DB
         value = [v.upper() for v in value]
