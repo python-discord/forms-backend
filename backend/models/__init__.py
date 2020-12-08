8e0a9a634e9bcc50ccad36ca1304e4e27f2992b1
ks129 <45097959+ks129@users.noreply.github.com>
2020-12-08 10:41:52 +0200

    Add AntiSpam and DiscordUser models to __init__.py

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,4 +1,6 @@
+from .antispam import AntiSpam
+from .discord_user import DiscordUser
 from .form import Form
 from .question import Question
 
-__all__ = ["Form", "Question"]
+__all__ = ["AntiSpam", "DiscordUser", "Form", "Question"]
