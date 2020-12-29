4bce32330ecd298e610e4249e327ddb502542844
ks129 <45097959+ks129@users.noreply.github.com>
2020-12-29 19:31:01 +0200

    Add default value to guild ID

--- backend/constants.py
+++ backend/constants.py
@@ -24,7 +24,7 @@ DOCS_PASSWORD = os.getenv("DOCS_PASSWORD")
 
 SECRET_KEY = os.getenv("SECRET_KEY", binascii.hexlify(os.urandom(30)).decode())
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-DISCORD_GUILD = os.getenv("DISCORD_GUILD")
+DISCORD_GUILD = os.getenv("DISCORD_GUILD", 267624335836053506)
 
 HCAPTCHA_API_SECRET = os.getenv("HCAPTCHA_API_SECRET")
 
